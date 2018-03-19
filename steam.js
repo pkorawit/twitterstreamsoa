1bd57391b9f3d54be0beac43d29760bef90f9810
Korawit <korawit.p@gmail.com>
2018-03-19 16:58:13 +0700

    Edit socket.io app

--- steam.js
+++ steam.js
@@ -2,7 +2,7 @@ var Twitter = require('node-tweet-stream');
 var express = require('express'); // Get the module
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(app);
 var t = new Twitter({
     consumer_key: 'm1U7OV4rD7Oko1FfysSzfc4pU',
     consumer_secret: 'agGdkI49lQHtU6mWZt1F19ZmB3KtK9SwHyidytezBWfD8Cjk0Y',
