c51274281d7c8d90246471355931e17d28b02ba6
Korawit <korawit.p@gmail.com>
2018-03-19 16:40:49 +0700

    Edit port

--- steam.js
+++ steam.js
@@ -25,7 +25,7 @@ io.on('connection', function (socket) {
 
 app.use("/",express.static(__dirname));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT  || 3000;
 
 app.listen(port, function () {
     console.log('listening on *:' + port);
